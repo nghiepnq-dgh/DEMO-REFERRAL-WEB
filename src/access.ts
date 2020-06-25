9c4d8564b12ca6a52705af703173db5f17dbe81a
HaoNN-DGH <haonn@digitechglobalco.com>
2020-06-25 22:59:52 +0700

    fix missing role

--- src/access.ts
+++ src/access.ts
@@ -3,11 +3,11 @@ import store from 'store'
 
 export default function() {
   const currentUser = store.get('currentUser');
-  const { role } = currentUser;
+  // const { role } = currentUser;
   
   return {
-    readProduct: role === 'CUSTOMER' || false,
-    payment: role === 'CUSTOMER' || false,
-    readCustomer: role === 'CLIENT' || false,
+    readProduct: currentUser && currentUser.role === 'CUSTOMER' || false,
+    payment: currentUser && currentUser.role === 'CUSTOMER' || false,
+    readCustomer: currentUser && currentUser.role === 'CLIENT' || false,
   };
 }
