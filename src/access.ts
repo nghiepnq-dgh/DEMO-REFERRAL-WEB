394533b04198a577325de3aecbaf97ac90f0ba2a
HaoNN-DGH <haonn@digitechglobalco.com>
2020-06-26 22:39:15 +0700

    fix missing role

--- src/access.ts
+++ src/access.ts
@@ -2,10 +2,11 @@ import { AccountRole, SystemRole, Privilege } from './utils/constants';
 import store from 'store'
 
 export default function() {
+
   const currentUser = store.get('currentUser');
-  // const { role } = currentUser;
   
   return {
+    // fix: check currentUser
     readProduct: currentUser && currentUser.role === 'CUSTOMER' || false,
     payment: currentUser && currentUser.role === 'CUSTOMER' || false,
     readCustomer: currentUser && currentUser.role === 'CLIENT' || false,
